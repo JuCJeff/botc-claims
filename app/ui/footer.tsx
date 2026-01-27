0d515a6167b6a8651bbc7f44c24451ac6e013b3f
Jeff Ma <jucjeff96@gmail.com>
2026-01-27 11:47:00 -0600

    style: modify footer styling

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className='w-full mt-auto py-6 bg-background'>
+    <footer className='w-full mt-auto py-6 bg-secondary'>
       <div className='container mx-auto px-4 text-center'>
         <p className='text-sm text-foreground'>
           © 2026 Jeff Ma - All rights reserved
